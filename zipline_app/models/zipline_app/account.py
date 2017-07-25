6bfe788ccd5619df9f6c126d65f8a7330393716c
Shadi Akiki <shadiakiki1986@gmail.com>
2017-07-25 15:01:04 +0300

    aesthetics

--- zipline_app/models/zipline_app/account.py
+++ zipline_app/models/zipline_app/account.py
@@ -16,3 +16,12 @@ class Account(models.Model):
     def delete(self):
       if self.order_set.count()>0:
         raise ValueError("Cannot delete account because it is linked to orders")
+
+#    def get_account_name_display(self):
+#      max_len = 10
+#      need_elipsis = len(self.account_name) > max_len
+#      return self.account_name[:max_len] + ('...' if need_elipsis else '')
+#
+#    def get_account_origin_display(self):
+#      map_origin = {'MF Lebanon': 'LEB', 'MF Dubai': 'DXB'}
+#      return map_origin[self.account_origin]
