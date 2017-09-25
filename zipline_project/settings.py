a3b50dd2abed23afb17f0b789dee8e4e5bf101cb
Shadi Akiki <shadiakiki1986@gmail.com>
2017-09-25 13:17:00 +0300

    move db.sqlite3 file into data folder for mounting on docker host

--- zipline_project/settings.py
+++ zipline_project/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'zipline_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
     }
 }
 
